cf4d0217ee1c80d2f2755f14ad6c931087332091
Литовченко Матвей Дмитриевич <200288@edu.psuti.ru>
2023-10-15 15:46:46 +0400

    Стартовый набор зависимостей

---
+++ src/main/java/com/example/simbirgocarscharing/SimbirGoCarScharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.simbirgocarscharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirGoCarScharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirGoCarScharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simbirgocarscharing/SimbirGoCarScharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simbirgocarscharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirGoCarScharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
