6c141355b1cd933c1d0465e6ecc19d97069ca49e
Литовченко Матвей Дмитриевич <200288@edu.psuti.ru>
2023-10-17 13:36:25 +0400

    Завершен этап первичной настройки, выполнен первый запуск

--- src/main/java/com/example/simbirgocarscharing/SimbirGoCarScharingApplication.java
+++ src/main/java/com/example/simbirgocarscharing/SimbirGoCarScharingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimbirGoCarScharingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimbirGoCarScharingApplication.class, args);
     }
-
 }
--- src/main/java/com/example/simbirgocarscharing/model/AbstractEntity.java
+++ src/main/java/com/example/simbirgocarscharing/model/AbstractEntity.java
@@ -1,4 +1,23 @@
 package com.example.simbirgocarscharing.model;
 
-public class AbstractEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.Id;
+
+@MappedSuperclass
+abstract class AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
